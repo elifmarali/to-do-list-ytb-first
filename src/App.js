7fe767515ba40863a2a9b00fc946d256460ac15c
Elif Marali <elifmarali00@gmail.com>
2023-02-24 15:40:26 +0300

    add react-bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>Hello World</div>
+        <Button>Hello</Button>
       </header>
     </div>
   );
