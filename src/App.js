5b98a9c391598fa0f8632b9c65a0e85811689490
Elif Marali <elifmarali00@gmail.com>
2023-02-26 12:51:08 +0300

    add icon buttons on page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from "./logo.svg";
 import React, { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
-
+import { ReactComponent as EditButton } from "./assets/EditButton.svg";
+import { ReactComponent as SaveButton } from "./assets/SaveButton.svg";
+import { ReactComponent as TrashButton } from "./assets/TrashButton.svg";
 function App() {
-  const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState("");
   const changeTodo = (e) => {
     setTodo(e.target.value);
   };
-
   const addTodo = () => {
     setTodoList((prevTodoList) => [...prevTodoList, todo]);
     setTodo("");
   };
-
-  useEffect(() => {
-    console.log(todoList);
-  }, [todoList]);
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
@@ -33,14 +29,32 @@ function App() {
       <div className="mt-3 w-50">
         <ul className="">
           {todoList.map((todoItem, index) => (
-            <li key={index} className="mt-2 list-group-item fs-4 ">
-              <div className="d-flex justify-content-between">
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-center justify-content-between"
+            >
+              <div className="d-flex ">
                 <Form.Check type="checkbox" />
-                <label htmlFor="" className="">
-                  {" "}
-                  {todoItem}
-                </label>
-                <div>Butonlar</div>
+                <label> {todoItem}</label>x
+              </div>
+              <div>
+                <EditButton
+                  width={20}
+                  height={20}
+                  className="me-2"
+                  style={{ cursor: "pointer" }}
+                ></EditButton>
+                <SaveButton
+                  width={20}
+                  height={20}
+                  className="me-2"
+                  style={{ cursor: "pointer" }}
+                ></SaveButton>
+                <TrashButton
+                  width={20}
+                  height={20}
+                  style={{ cursor: "pointer" }}
+                ></TrashButton>
               </div>
             </li>
           ))}
